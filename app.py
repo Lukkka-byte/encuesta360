93cbb3f557d01393024ae9045a05071b5ea3ea64
Lucas Perez <lucaseptissera@gmail.com>
2025-05-07 18:54:13 +0200

    Fix: configurar host y puerto para Render

--- app.py
+++ app.py
@@ -223,4 +223,5 @@ def exportar_csv():
     return output
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
